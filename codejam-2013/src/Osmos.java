d5f9acc91cdccdb2ae60bb564d110fddb1f9002f
Jason <jason.056@gmail.com>
2013-05-17 02:06:26 -0700

    Added memoization, works for large testcases

--- codejam-2013/src/Osmos.java
+++ codejam-2013/src/Osmos.java
@@ -1,15 +1,16 @@
 import java.io.FileInputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 
 public class Osmos {
 
     public static void main(String args[]) throws Exception {
-        FileInputStream fis = new FileInputStream("src/OsmosInput.txt");
+        FileInputStream fis = new FileInputStream("resources/OsmosInputLarge.txt");
         Scanner scanner = new Scanner(fis);
         int testcase = 1;
         
@@ -17,13 +18,14 @@ public class Osmos {
         while(scanner.hasNextLine()) {
             int mote = Integer.valueOf(scanner.nextLine().split(" ")[0]);
             List<Integer> motelist = new ArrayList<Integer>();
+            Map<Integer, Map<Integer, Integer>> memos = new HashMap<Integer, Map<Integer, Integer>>();
             for(String s : scanner.nextLine().split(" ")) motelist.add(Integer.valueOf(s));
             Collections.sort(motelist);
-            System.out.println(String.format("Case #%d: %d", testcase++, consume(mote, 0, motelist)));
+            System.out.println(String.format("Case #%d: %d", testcase++, consume(mote, 0, motelist, memos)));
         }
     }
 
-    private static int consume(int mote, int lpos, List<Integer> motelist) {
+    private static int consume(int mote, int lpos, List<Integer> motelist, Map<Integer, Map<Integer, Integer>> memos) {
 
         // consume as much as possible
         while (lpos <= motelist.size()-1 && mote > motelist.get(lpos)) {
@@ -34,12 +36,28 @@ public class Osmos {
         if (mote == 1) return motelist.size();
         if (lpos > motelist.size()-1) return 0; //no more motes
         
-        int addres = consume(mote+mote-1, lpos, motelist);
-        if (addres == 0) return 1; //adding is the best option, no point in removing
-        int removeres = consume(mote, lpos+1, motelist);
+        int addresult;
+        if (memos.get(mote+mote-1) == null || memos.get(mote+mote-1).get(lpos) == null) {
+            addresult = consume(mote+mote-1, lpos, motelist, memos);
+            // need to create map first time through
+            if (memos.get(mote+mote-1) == null) memos.put(mote+mote-1, new HashMap<Integer, Integer>());
+            memos.get(mote+mote-1).put(lpos, addresult);
+        } else {
+            addresult = memos.get(mote+mote-1).get(lpos);
+        }
+        
+        int removeresult;
+        if (memos.get(mote) == null || memos.get(mote).get(lpos+1) == null) {
+            removeresult = consume(mote, lpos+1, motelist, memos);
+            // need to create map first time through
+            if (memos.get(mote) == null) memos.put(mote, new HashMap<Integer, Integer>());
+            memos.get(mote).put(lpos+1, removeresult);
+        } else {
+            removeresult = memos.get(mote).get(lpos+1);
+        }
+        
         //System.out.println("addres " + addres + ", removeres " + removeres);
-        return 1 + Math.min(addres, removeres);
+        return 1 + Math.min(addresult, removeresult);
 
     }
-
 }
